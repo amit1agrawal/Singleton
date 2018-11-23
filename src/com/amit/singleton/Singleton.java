8ae9d91313c741ffc8d85f293d8f33c2eb730a3e
Amit Agrawal <amitag88@in.ibm.com>
2018-11-23 18:14:14 +0530

    Initial commit

---
+++ src/com/amit/singleton/Singleton.java
@@ -0,0 +1,45 @@
+package com.amit.singleton;
+import java.io.Serializable;
+
+public class Singleton implements Serializable {		
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -9000752106687223004L;
+
+	private static Singleton instance = null;
+	
+	private Singleton(){
+		if( Singleton.instance != null ) {
+	        throw new InstantiationError( "Creating of this object is not allowed." );
+	    }
+	}
+	
+	public static Singleton getInstance(){
+		if (instance != null){
+			return instance;
+		}
+		synchronized (instance){
+			if (instance != null){
+				return instance;
+			}
+			else{
+				instance = new Singleton();
+			}			
+		}
+		return instance;
+	}
+	
+	protected Object Clone() throws CloneNotSupportedException{
+		throw new CloneNotSupportedException("Cloning of this object is not supported!");
+	}
+	
+	protected Object readResolve(){
+		return instance;
+	}
+	
+	public Object newInstance() throws InstantiationException {
+		throw new InstantiationException("Creating of this object is not allowed");
+	}
+}
+
